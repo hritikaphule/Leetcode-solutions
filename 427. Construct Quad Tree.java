886681e78d8fe18b58dba02f63a35953f056664d
hritikaphule <hritika.phule16@gmail.com>
2025-02-19 18:28:41 -0800

    LC 427

---
+++ 427. Construct Quad Tree.java
@@ -0,0 +1,71 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+    public Node construct(int[][] grid) {
+        return constructHelper(grid, 0, 0, grid.length);
+    }
+
+    private Node constructHelper(int[][] grid, int row, int col, int size) {
+        if (isUniform(grid, row, col, size)) {
+            return new Node(grid[row][col] == 1, true);
+        }
+
+        int newSize = size / 2;
+        Node topLeft = constructHelper(grid, row, col, newSize);
+        Node topRight = constructHelper(grid, row, col + newSize, newSize);
+        Node bottomLeft = constructHelper(grid, row + newSize, col, newSize);
+        Node bottomRight = constructHelper(grid, row + newSize, col + newSize, newSize);
+
+        return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
+    }
+
+    private boolean isUniform(int[][] grid, int row, int col, int size) {
+        int val = grid[row][col];
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+                if (grid[i][j] != val) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
